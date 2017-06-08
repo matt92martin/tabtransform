6e946262b7a04fcbc8624d1272a6cfe148e379c8
Matt Martin <matt92martin@gmail.com>
2017-06-08 08:31:44 -0700

    init

---
+++ tabtrans.py
@@ -0,0 +1,7 @@
+
+# Example file
+if Q1 == 1:
+    row.Q1 = 'example 1'
+    row.Q0 = 'example 0'
+    row.Q10 = 'example 10'
+    row.uuid = 'example uuid'
---
+++ tabtransform.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import sys, os, traceback, argparse, textwrap
+import logging as log
+from csv import DictReader
+# from guppy import hpy
+
+class Row(object):
+    """
+    Row Object
+    """
+
+class Main:
+
+    def __init__(self, opts):
+        self.opts = opts
+        self.reader = self.load_file()
+        self.reader2 = self.load_file()
+        self.fieldnames = self.reader.fieldnames
+        self.log = log.basicConfig(format="%(message)s", level=log.INFO)
+
+        if not os.path.isfile('tabtrans.py'):
+            log.info("Define a tabtrans.py")
+            sys.exit(0)
+
+
+    def load_file(self):
+        return DictReader(open(self.opts.file, 'rb'), delimiter="\t")
+
+    def gen_line(self):
+        code = compile(open('tabtrans.py').read(), 'tabtrans.py', 'exec')
+
+        extra = {}
+        fieldnames = set(self.fieldnames)
+        seenvariables = set()
+        for line in self.reader:
+
+            d = line.copy()
+            for k,v in d.iteritems():
+                if v.isdigit():
+                    d[k] = int(v)
+
+            d['row'] = Row()
+            exec(code, d)
+            extra[self.reader.line_num] = vars( d['row'] )
+            seenvariables.update( vars( d['row'] ).keys() )
+
+        self.fieldnames += list(seenvariables - fieldnames)
+
+        print '\t'.join(self.fieldnames)
+        for line in self.reader2:
+            line.update(extra[self.reader.line_num])
+            yield '\t'.join([line.get(var, '') for var in self.fieldnames])
+
+
+    def main(self):
+        g = self.gen_line()
+        for line in g:
+            print line
+
+
+def options():
+    parser = argparse.ArgumentParser(
+        description=textwrap.dedent( '''
+            Command:
+            tabtransform <file>
+        ''' ),
+        add_help=False,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument( '--help', help=argparse.SUPPRESS, action='help' )
+    parser.add_argument( 'file', help='File to parse', type=str )
+
+    return parser.parse_args( )
+
+
+
+
+if __name__ == '__main__':
+    try:
+        main = Main( options() )
+        # h = hpy()
+        # print h.heap()
+        sys.exit( main.main() )
+    except KeyboardInterrupt, e:
+        raise e
+    except SystemExit, e:
+        raise e
+    except Exception, e:
+        print 'ERROR, UNEXPECTED EXCEPTION'
+        print str(e)
+        traceback.print_exc()
+        os._exit(1)
